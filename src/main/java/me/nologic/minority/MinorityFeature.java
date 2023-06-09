3a6b7317938b79dd894bec9e8d024c3c0823b684
NoLogic <pixel2374@gmail.com>
2023-06-09 08:37:46 +0600

    Fixed auto-initialization for configs

--- src/main/java/me/nologic/minority/MinorityFeature.java
+++ src/main/java/me/nologic/minority/MinorityFeature.java
@@ -1,10 +1,15 @@
 package me.nologic.minority;
 
 import lombok.SneakyThrows;
+import me.nologic.minority.annotations.Configurable;
+import me.nologic.minority.annotations.ConfigurationKey;
 import me.nologic.minority.annotations.Translatable;
 import me.nologic.minority.annotations.TranslationKey;
+import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.File;
 import java.lang.reflect.Field;
+import java.util.Objects;
 
 public interface MinorityFeature {
 
@@ -23,10 +28,27 @@ public interface MinorityFeature {
                     final TranslationKey key = field.getAnnotation(TranslationKey.class);
                     final String path = key.section() + plugin.getLanguage().options().pathSeparator() + key.name();
                     field.setAccessible(true);
-                    plugin.getLogger().info(String.format("Trying to change field %s value to %s.", field.getName(), plugin.getLanguage().get(path)));
                     field.set(object, plugin.getLanguage().get(path));
-                    plugin.getLogger().info(String.format("New value in field %s: %s.", field.getName(), field.get(object)));
+                }
+            }
+        }
 
+        // 2. If class have @Configurable annotation, look for fields which annotated with @ConfigurationKey
+        if (clazz.isAnnotationPresent(Configurable.class)) {
+            final Configurable section = clazz.getAnnotation(Configurable.class);
+            for (Field field : clazz.getDeclaredFields()) {
+                if (field.isAnnotationPresent(ConfigurationKey.class)) {
+                    final ConfigurationKey key = field.getAnnotation(ConfigurationKey.class);
+
+                    final YamlConfiguration config = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), section.file()));
+                    String path = section.path() + config.options().pathSeparator() + key.path();
+                    switch (key.type()) {
+                        case STRING -> field.set(object, config.getString(path));
+                        case BOOLEAN -> field.setBoolean(object, config.getBoolean(path));
+                        case INTEGER -> field.setInt(object, config.getInt(path));
+                        case DOUBLE -> field.setDouble(object, config.getDouble(path));
+                        case ENUM -> field.set(object, Enum.valueOf(field.getType().asSubclass(Enum.class), Objects.requireNonNull(config.getString(path)))); // enum used for automatic cast, for example if we're using Material or EntityType as fields
+                    }
                 }
             }
         }
--- src/main/java/me/nologic/minority/config/ConfigurationWizard.java
+++ src/main/java/me/nologic/minority/config/ConfigurationWizard.java
@@ -67,43 +67,6 @@ public class ConfigurationWizard {
             }
         }
 
-        // TODO: Separate instance creation and event handler registration in other method. It shouldn't be automatic.
-        // 5. Now, it's time to create a new instance of our class and assign all fields that have the @Key annotation with values from generated/loaded configuration.
-        MinorityFeature instance = feature.getConstructor().newInstance();
-        for (Field field : instance.getClass().getDeclaredFields()) {
-            if (field.isAnnotationPresent(ConfigurationKey.class)) {
-                ConfigurationKey key = field.getAnnotation(ConfigurationKey.class);
-
-                // TODO: Add support to arrays.
-                // Parse the value and assign it to the marked field!
-                // Very important to read the values from a newly generated config, not from an annotation above!
-                // Also, we can skip checks for null pointers, because we ALWAYS have the config with the paths that we're need.
-                String path = section.path() + config.options().pathSeparator() + key.path();
-                switch (key.type()) {
-                    case STRING -> field.set(instance, config.getString(path));
-                    case BOOLEAN -> field.setBoolean(instance, config.getBoolean(path));
-                    case INTEGER -> field.setInt(instance, config.getInt(path));
-                    case DOUBLE -> field.setDouble(instance, config.getDouble(path));
-                    case ENUM -> field.set(instance, Enum.valueOf(field.getType().asSubclass(Enum.class), Objects.requireNonNull(config.getString(path)))); // enum used for automatic cast, for example if we're using Material or EntityType as fields
-                }
-            }
-        }
-
-        // 6. Right after that, we try to find out is our class have @EventHandler. If true, it will be considered as Listener.
-        boolean isListener = false;
-        for (Method method : instance.getClass().getDeclaredMethods()) {
-            if (method.isAnnotationPresent(EventHandler.class)) {
-                plugin.getLogger().info("Listener detected: " + instance.getClass());
-                isListener = true;
-            }
-        }
-
-        // 7. If iterating class is Listener, we should automatically register it in Bukkit plugin manager.
-        if (isListener) {
-            plugin.getLogger().info("Detected the @EventHandler annotation usage in the " + instance.getClass().getName() + "class. Registering it as Listener.");
-            plugin.getServer().getPluginManager().registerEvents((Listener) instance, plugin);
-        }
-
         // 9. And after all the job is done, we shouldn't forget to save the config file!
         config.save(file);
 
